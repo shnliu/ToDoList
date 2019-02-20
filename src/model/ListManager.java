bb3f8e73f3d82a104f72cdd1c0abdabaffaa2d3e
Sara L <n7o0b@ugrad.cs.ubc.ca>
2019-02-19 21:23:58 -0800

    working on todolist class and listmanager

--- src/model/ListManager.java
+++ src/model/ListManager.java
@@ -1,4 +1,14 @@
 package model;
 
 public class ListManager {
+    private CurrentList currentList; // has 3 items
+    private CompletedList completedList; // has all completed items
+    private AllDoList allDoList; //has all to do items
+
+    public ListManager() {
+        currentList = new CurrentList();
+        completedList = new CompletedList();
+        allDoList = new AllDoList();
+    }
+
 }
--- src/model/ToDoList.java
+++ src/model/ToDoList.java
@@ -1,4 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoList {
+    private String name;
+    private List<Item> listOfItems;
+
+    public ToDoList() {
+        this.name = name;
+        listOfItems = new ArrayList<Item>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void addItem(Item item){
+        this.listOfItems.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.listOfItems.remove(item);
+    }
 }
